2f346c113b66f44fa0bacf6ede9a380ca43e9cd2
Varun Arora <varunarora.mct@gmail.com>
2014-06-18 01:05:26 -0700

    Init venv

---
+++ venv/lib/python2.7/site-packages/weasyprint/compat.py
@@ -0,0 +1,98 @@
+# coding: utf8
+"""
+    weasyprint.compat
+    -----------------
+
+    Workarounds for compatibility with Python 2 and 3 in the same code base.
+
+    :copyright: Copyright 2011-2014 Simon Sapin and contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+
+"""
+
+from __future__ import division, unicode_literals
+
+import sys
+import email
+
+
+__all__ = ['Request', 'base64_decode', 'base64_encode', 'basestring',
+           'ints_from_bytes', 'iteritems', 'izip', 'parse_email', 'parse_qs',
+           'pathname2url', 'quote', 'unicode', 'unquote', 'unquote_to_bytes',
+           'urlencode', 'urljoin', 'urlopen', 'urlopen_contenttype',
+           'urlparse_uses_relative', 'urlsplit', 'xrange']
+
+
+if sys.version_info[0] >= 3:
+    # Python 3
+    from urllib.parse import (
+        urljoin, urlsplit, quote, unquote, unquote_to_bytes, parse_qs,
+        urlencode, uses_relative as urlparse_uses_relative)
+    from urllib.request import urlopen, Request, pathname2url
+    from array import array
+    from base64 import (decodebytes as base64_decode,
+                        encodebytes as base64_encode)
+
+    unicode = str
+    basestring = str
+    xrange = range
+    iteritems = dict.items
+    izip = zip
+
+    def urlopen_contenttype(url):
+        """Return (file_obj, mime_type, encoding)"""
+        result = urlopen(url)
+        info = result.info()
+        mime_type = info.get_content_type()
+        charset = info.get_param('charset')
+        return result, mime_type, charset
+
+    def parse_email(data):
+        if isinstance(data, bytes):
+            data = data.decode('utf8')
+        return email.message_from_string(data)
+
+    def ints_from_bytes(byte_string):
+        """Return a list of ints from a byte string"""
+        return list(byte_string)
+
+else:
+    # Python 2
+    from urlparse import (urljoin, urlsplit, parse_qs,
+                          uses_relative as urlparse_uses_relative)
+    from urllib2 import urlopen, Request
+    from urllib import pathname2url, quote, unquote, urlencode
+    from array import array as _array
+    from itertools import izip, imap
+    from base64 import (decodestring as base64_decode,
+                        encodestring as base64_encode)
+
+    unicode = unicode
+    basestring = basestring
+    xrange = xrange
+    iteritems = dict.iteritems
+
+    def array(typecode, initializer):
+        return _array(typecode.encode('ascii'), initializer)
+
+    def urlopen_contenttype(url):
+        """Return (file_obj, mime_type, encoding)"""
+        result = urlopen(url)
+        info = result.info()
+        mime_type = info.gettype()
+        charset = info.getparam('charset')
+        return result, mime_type, charset
+
+    def unquote_to_bytes(data):
+        if isinstance(data, unicode):
+            data = data.encode('ascii')
+        return unquote(data)
+
+    def parse_email(data):
+        if isinstance(data, unicode):
+            data = data.encode('utf8')
+        return email.message_from_string(data)
+
+    def ints_from_bytes(byte_string):
+        """Return a list of ints from a byte string"""
+        return imap(ord, byte_string)
---
+++ venv/lib/python2.7/site-packages/weasyprint/tests/testing_utils.py
@@ -0,0 +1,99 @@
+# coding: utf8
+"""
+    weasyprint.tests.testing_utils
+    ------------------------------
+
+    Helpers for tests.
+
+    :copyright: Copyright 2011-2014 Simon Sapin and contributors, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+
+"""
+
+from __future__ import division, unicode_literals, print_function
+
+import sys
+import os.path
+import logging
+import contextlib
+import functools
+
+from .. import HTML, CSS
+from ..logger import LOGGER
+
+
+# TODO: find a way to not depend on a specific font
+FONTS = 'Liberation Sans, Arial'
+
+TEST_UA_STYLESHEET = CSS(filename=os.path.join(
+    os.path.dirname(__file__), '..', 'css', 'tests_ua.css'
+))
+
+
+class TestHTML(HTML):
+    """Like weasyprint.HTML, but with a lighter UA stylesheet."""
+    def _ua_stylesheets(self):
+        return [TEST_UA_STYLESHEET]
+
+
+def resource_filename(basename):
+    """Return the absolute path of the resource called ``basename``."""
+    return os.path.join(os.path.dirname(__file__), 'resources', basename)
+
+
+class CallbackHandler(logging.Handler):
+    """A logging handler that calls a function for every message."""
+    def __init__(self, callback):
+        logging.Handler.__init__(self)
+        self.emit = callback
+
+
+@contextlib.contextmanager
+def capture_logs():
+    """Return a context manager that captures all logged messages."""
+    logger = LOGGER
+    messages = []
+
+    def emit(record):
+        message = '%s: %s' % (record.levelname.upper(), record.getMessage())
+        messages.append(message)
+
+    previous_handlers = logger.handlers
+    logger.handlers = []
+    logger.addHandler(CallbackHandler(emit))
+    try:
+        yield messages
+    finally:
+        logger.handlers = previous_handlers
+
+
+def assert_no_logs(function):
+    """Decorator that asserts that nothing is logged in a function."""
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        with capture_logs() as logs:
+            try:
+                function(*args, **kwargs)
+            except Exception:  # pragma: no cover
+                if logs:
+                    print('%i errors logged:' % len(logs), file=sys.stderr)
+                    for message in logs:
+                        print(message, file=sys.stderr)
+                raise
+            else:
+                if logs:
+                    print('%i errors logged:' % len(logs))
+                    for message in logs:
+                        print(message, file=sys.stderr)
+                    assert 0
+    return wrapper
+
+
+def almost_equal(a, b):
+    if (isinstance(a, list) and isinstance(b, list)
+            or isinstance(a, tuple) and isinstance(b, tuple)):
+        return len(a) == len(b) and all(
+            almost_equal(aa, bb) for aa, bb in zip(a, b))
+    if isinstance(a, float) or isinstance(b, float):
+        return round(abs(a - b), 6) == 0
+    return a == b
